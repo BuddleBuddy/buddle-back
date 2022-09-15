23b0f769bf4867643dfbeb5f0d32fb27e6fd4a5e
KeonhoPark <gse04063@gmail.com>
2022-09-16 03:37:28 +0900

    feat: 구글 로그인(Role, UserRepository 추가)

--- src/main/java/buddle/buddlebuddy/user/User.java
+++ src/main/java/buddle/buddlebuddy/user/User.java
@@ -3,6 +3,7 @@ package buddle.buddlebuddy.user;
 import buddle.buddlebuddy.basket.Basket;
 import buddle.buddlebuddy.like.Likes;
 import lombok.*;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
 import java.util.List;
@@ -24,5 +25,9 @@ public class User {
 
     @OneToMany(fetch = FetchType.LAZY)
     private List<Likes> likesList;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
 
 }
